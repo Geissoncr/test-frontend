74192991b08f4327705264d62c3f3a085cc63ca3
Geissoncr <geissoncr@gmail.com>
2019-06-15 10:02:53 -0300

    Teste de Commit

--- src/container/Home.js
+++ src/container/Home.js
@@ -25,67 +25,63 @@ class Home extends Component {
       timeData: resultado3.data
     });
   }
-200  
+  200;
   montaTela() {
     const optionsAnual = {
-        responsive: true,
-        legend: { display: false},
-        title: {
-            display: true,
-            text: "BARS CHART",
-            position: 'top',
-            fontFamily: "arial",
-            fontSize: 20,
-            
-          }
+      responsive: true,
+      legend: { display: false },
+      title: {
+        display: true,
+        text: "Grafico de Barras",
+        position: "top",
+        fontFamily: "arial",
+        fontSize: 20
+      }
     };
     const optionsPercentage = {
-        responsive: true,
-        legend: { display: false},
-        title: {
-            display: true,
-            text: "BARS CHART",
-            position: 'top',
-            fontFamily: "arial",
-            fontSize: 20,
-            
-          }
+      responsive: true,
+      legend: { display: false },
+      title: {
+        display: true,
+        text: "BARS CHART",
+        position: "top",
+        fontFamily: "arial",
+        fontSize: 20
+      }
     };
     const dataAnual = {
-        labels: this.state.anualResults.map( label => label.label.substring(0,3)),
-        datasets: [{
-        label: "Bars Chart",
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: this.state.anualResults.map(valor => valor.value),
-        }]
+      labels: this.state.anualResults.map(label => label.label.substring(0, 3)),
+      datasets: [
+        {
+          label: "Bars Chart",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: this.state.anualResults.map(valor => valor.value)
+        }
+      ]
     };
     const dataPie = {
-        labels: this.state.anualPercentage.map(label => label.label),
-        datasets: [
-            {
-              data: this.state.anualPercentage.map(valor=>valor.value),
-            backgroundColor: [
-              "#ABE1FA",
-              "#32B88B",
-              "#2C82BE"
-              ]
-            }
-          ]
-    }
-    return( 
-        <div style={{ width: "600px" }}>
+      labels: this.state.anualPercentage.map(label => label.label),
+      datasets: [
+        {
+          data: this.state.anualPercentage.map(valor => valor.value),
+          backgroundColor: ["#ABE1FA", "#32B88B", "#2C82BE"]
+        }
+      ]
+    };
+    return (
+      <div style={{ width: "600px" }}>
         <Bar data={dataAnual} options={optionsAnual} />
-        <Pie data={dataPie} options={optionsPercentage}/>
-        </div>
-        );
+        <Pie data={dataPie} options={optionsPercentage} />
+      </div>
+    );
   }
 
   render() {
     return this.state.anualResults.length > 0 ? (
       <div> {this.montaTela()}</div>
     ) : (
-      <div>Preparando os Dados ....</div>
+      <div>Preparando os Dados .............</div>
     );
     //   <div style={{ width: "1000px" }}>
     //     <p>Lista de Retorno: </p>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 // import App from './App';
-import Home from './container/Home';
-import * as serviceWorker from './serviceWorker';
+import Home from "./container/Home";
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<Home />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
